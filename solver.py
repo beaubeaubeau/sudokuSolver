bf10e8ba7eec05871750eb4e876661061cba9f2c
Beau Si <bs655@cornell.edu>
2020-07-11 00:12:39 -0500

    Added nice user display of Sudoku board

--- solver.py
+++ solver.py
@@ -15,9 +15,32 @@ function backtrack(assignment, constraintProblem):
     return failure
 """
 
-board = [[1,-1,3,2],
-        [-1,-1,1,4],
-        [4,1,2,-1],
-        [2,3,-1,-1]]
-for row in board:
-    print(row)
+# Board to solve. 0 denotes a blank or unknown value
+# Starting with a simple Sudoku board as a warm up
+board = [[1,0,3,2],
+        [0,0,1,4],
+        [4,1,2,0],
+        [2,3,0,0]]
+
+# Possible values that each element in the Sudoku board can take on
+VALUES = [1,2,3,4]
+
+# Prints the Sudoku board in a user friendly way in the output box
+def printBoard(board):
+    for i in range(len(board)):
+        row = board[i]
+        disp = ""
+        for j in range(len(row)):
+            item = row[j]
+            disp = disp+str(item)
+            # for the smaller sub boxes
+            if(j == 1):
+                disp = disp + "|"
+            else:
+                disp = disp + " "
+        print(disp)
+        # for the smaller sub boxes
+        if(i == 1):
+            print("- - - - ")
+
+printBoard(board)
