2d09fdd6bcc319ef771a657c6ed57310dfdee48f
Beau Si <bs655@cornell.edu>
2020-07-14 17:47:59 -0500

    Added method to reset board to original values. Added isValid to check if new value assignment is a legal sudoku move

--- solver.py
+++ solver.py
@@ -15,21 +15,40 @@ function backtrack(assignment, constraintProblem):
     return failure
 """
 
-# Board to solve. 0 denotes a blank or unknown value
-# Starting with a simple Sudoku board as a warm up
-board = [[1,0,3,2],
+
+"""
+Board to solve. 0 denotes a blank or unknown value.
+Starting with a simple Sudoku board as a warm up
+"""
+board = [[1,0,3,0],
         [0,0,1,4],
         [4,1,2,0],
-        [2,3,0,0]]
+        [0,3,0,0]]
 
-# Possible values that each element in the Sudoku board can take on
-VALUES = [1,2,3,4]
+def resetBoard(board: list):
+    """
+    Resets the board to its original values.
 
-# Prints the Sudoku board in a user friendly way in the output box
-def printBoard(board):
+    @param board:list
+        List of lists representing the Sudoku board of size 4x4.
+    """
+    board = [[1,0,3,0],
+            [0,0,1,4],
+            [4,1,2,0],
+            [0,3,0,0]]
+
+
+def printBoard(board: list):
+    """
+    Prints the Sudoku board in a user friendly way in the output box.
+
+    @param board:list
+        List of lists representing the Sudoku board of size 4x4.
+    """
     for i in range(len(board)):
         row = board[i]
         disp = ""
+        # Displays each element in the row, adding vertical bar where necessary
         for j in range(len(row)):
             item = row[j]
             disp = disp+str(item)
@@ -39,8 +58,64 @@ def printBoard(board):
             else:
                 disp = disp + " "
         print(disp)
-        # for the smaller sub boxes
+        # Adds row of dividing lines to separate the smaller boxes
         if(i == 1):
             print("- - - - ")
 
-printBoard(board)
+#printBoard(board)
+
+
+def isValid(board: list, row: int, col: int) -> bool:
+    """
+    Checks to see if the value at the specified row and column of
+    the sudoku board is valid.
+    The value assigned to the specified row and column must be
+    non-zero values.
+    The board must be dimension nxn.
+
+    @param board: list
+        List of lists representing the Sudoku board of size 4x4.
+    @param row: int
+        Integer value representing the row number (0 indexed).
+    @param col: int
+        Integer value representing the column number (0 indexed).
+
+    @return
+        True if value at specified position in board is valid.
+        False otherwise.
+    """
+    assert len(board)==len(board[0])
+    assert len(board)==4
+
+    currVal = board[row][col]
+    # First check the row. Make sure new assigned value does not already exist.
+    for i in range(len(board)):
+        if(i!=col and board[row][i] == currVal):
+            print("FAILED ROW")# for debugging
+            return False
+
+    # Check the column, ensuring newly assigned value doesn't already exist.
+    for i in range(len(board)):
+        if(i!= row and board[i][col]==currVal):
+            print("FAILED COL")# for debugging
+            return False
+
+    # Check the small box, ensuring newly assigned value doesn't already exist.
+    # Determining the boundaries of the smaller box.
+    startRow = 0
+    startCol = 0
+
+    if row > 1:
+        startRow = 2
+    if col > 1:
+        startCol = 2
+
+    stopRow = startRow + 2
+    stopCol = startCol + 2
+
+    for i in range(startRow, stopRow):
+        for j in range(startCol,stopCol):
+            if(i!=row and j!=col and board[i][j]==currVal):
+                print("FAILED BOX") # for debugging
+                return False
+    return True
